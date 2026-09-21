4a2c0e4cccdde92a33f5a12df4e18d83203f0ea2
adityasawant01 <adityasawant767@gmail.com>
2026-09-21 17:28:44 +0530

    loops

--- first.java
+++ first.java
@@ -1,11 +1,38 @@
+import java.util.Scanner;
 
 class First{
     public static void main(String[] args) {
         
-        
+        int i = 0;
+        while(i < 11){
+          System.out.println(i);
+          i++;
+        }
         for(int a = 0; a <= 10; a ++ ){
         System.out.println("hello");
-           
-        }
     }
+    int b = 1;
+    do{
+        System.out.println(b);
+        b++;
+    }
+    while(b <= 10);
+
+
+    //add sum of n natural numbers: by for loop
+
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    
+    for(int c = 1; c <= 100; c++){
+        System.out.println(c*n);
+    }
+
+    
+
+    
+    
+
+
+    } 
 }
