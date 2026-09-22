d011e8dcd5de18b2021f4e44a6d13d4715b6dac3
adityasawant01 <adityasawant767@gmail.com>
2026-09-22 17:19:19 +0530

    functions

--- first.java
+++ first.java
@@ -1,38 +1,16 @@
-import java.util.Scanner;
+import java.util.*;
 
-class First{
-    public static void main(String[] args) {
-        
-        int i = 0;
-        while(i < 11){
-          System.out.println(i);
-          i++;
-        }
-        for(int a = 0; a <= 10; a ++ ){
-        System.out.println("hello");
+class First {
+    public static void print_my_name(String name){
+System.out.println(name);
+return;
     }
-    int b = 1;
-    do{
-        System.out.println(b);
-        b++;
-    }
-    while(b <= 10);
-
-
-    //add sum of n natural numbers: by for loop
-
+   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    
-    for(int c = 1; c <= 100; c++){
-        System.out.println(c*n);
-    }
+    String name = sc.next();
+    print_my_name(name);
 
     
-
+   }
     
-    
-
-
-    } 
 }
